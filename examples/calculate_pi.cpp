63641cdcff9e902b16750dd5086be247212f2faa
chenlighten <xushichen@mail.ustc.edu.cn>
2023-11-01 15:19:04 -0400

    Reformat

--- examples/calculate_pi.cpp
+++ examples/calculate_pi.cpp
@@ -1,21 +1,23 @@
-#include <vector>
-#include <ranges>
+#include <iostream>
 #include <memory>
 #include <random>
-#include <iostream>
+#include <ranges>
+#include <vector>
 #include "rdd.h"
 
 int main() {
-    std::random_device random_engine;
-    std::uniform_real_distribution<double> distribution(-1, 1);
+  std::random_device random_engine;
+  std::uniform_real_distribution<double> distribution(-1, 1);
 
-    // Parallelly calculate the value of pi by monte carlo algorithm with n samples.
-    int n = 1'000'000;
-    double pi =
-        (cpark::GeneratorRdd(0, n, [&](auto) -> double {
-            double x = distribution(random_engine), y = distribution(random_engine);
-            return x * x + y * y <= 1 ? 1 : 0;
-        }) | cpark::Reduce([](auto x, auto y){return x + y;})) / n * 4;
+  // Parallelly calculate the value of pi by monte carlo algorithm with n samples.
+  int n = 1000000;
+  double pi = (cpark::GeneratorRdd(0, n,
+                                   [&](auto) -> double {
+                                     double x = distribution(random_engine), y = distribution(random_engine);
+                                     return x * x + y * y <= 1 ? 1 : 0;
+                                   }) |
+               cpark::Reduce([](auto x, auto y) { return x + y; })) /
+              n * 4;
 
-    std::cout << "The value of pi is roughly " << pi << std::endl;
+  std::cout << "The value of pi is roughly " << pi << std::endl;
 }
