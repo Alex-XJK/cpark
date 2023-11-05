976f188060772f8cdc8e92dc70102d177b0ba936
Alex Jiakai Xu <ax2155@columbia.edu>
2023-11-05 14:37:20 -0500

    Tryout filter_rdd

---
+++ include/filter_rdd.h
@@ -0,0 +1,13 @@
+#ifndef CPARK_FILTER_RDD_H
+#define CPARK_FILTER_RDD_H
+
+#include <vector>
+
+#include "base_rdd.h"
+#include "utils.h"
+
+namespace cpark {
+
+}  // namespace cpark
+
+#endif  //CPARK_FILTER_RDD_H
