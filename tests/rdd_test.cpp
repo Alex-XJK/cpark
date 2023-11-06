39b27e74fedb72d2a9afbbb0bc71716190b7a88a
Alex Jiakai Xu <ax2155@columbia.edu>
2023-11-06 15:11:20 -0500

    Add gtest for filter rdd

--- tests/rdd_test.cpp
+++ tests/rdd_test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <thread>
 #include "gtest/gtest.h"
+
 #include "cpark.h"
 #include "plain_rdd.h"
+#include "generator_rdd.h"
+#include "filter_rdd.h"
 
 using namespace cpark;
 
@@ -23,12 +26,27 @@ TEST(config, thread_size) {
     EXPECT_EQ(1000, plain_rdd_2.size());
 }
 
-TEST(rdd_suite, plain_rdd) {
-    Config default_config;
-    ExecutionContext default_context{default_config};
+TEST(rdd_suite, plain_rdd_dummy) {
+    ExecutionContext default_context{};
     auto transformed_iota_view =
         std::views::iota(1, 100 + 1) | std::views::transform([](auto x) { return x * x; });
     concepts::Rdd auto plain_rdd = PlainRdd(transformed_iota_view, &default_context);
 
     EXPECT_EQ(1, 1);
+}
+
+TEST(rdd_suite, filter_rdd_correctness) {
+    ExecutionContext default_context{};
+    auto even = [](int i) { return 0 == i % 2; };
+    auto generator_rdd = GeneratorRdd(0, 1000 + 1, [](auto x) { return x; }, &default_context);
+    auto filter_rdd = FilterRdd(generator_rdd, even);
+    int sum = 0, size = 0;
+    for (const concepts::Split auto& split : filter_rdd) {
+        for (const auto &x: split) {
+            sum += x;
+            size += 1;
+        }
+    }
+    EXPECT_EQ(501, size);
+    EXPECT_EQ(250500, sum);
 }
