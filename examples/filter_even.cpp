83c0d35372c23e19a0c31a0308837be2e77156f6
Alex Jiakai Xu <ax2155@columbia.edu>
2023-11-06 14:50:48 -0500

    Improve example use case

--- examples/filter_even.cpp
+++ examples/filter_even.cpp
@@ -1,53 +1,43 @@
 #include <iostream>
-#include <ranges>
-#include <variant>
 
 #include "filter_rdd.h"
 #include "generator_rdd.h"
 
+template <cpark::concepts::Rdd R>
+void printRdd(R rdd) {
+    for (const cpark::concepts::Split auto& split : rdd) {
+        for (const auto &x: split)
+            std::cout << x << "\t";
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
 int main() {
   using namespace cpark;
 
-  // Simple use cases of cpark components.
-
-  // Creates configuration objects.
-  Config default_config;
-  Config customized_config =
-      Config().setDebugName("My CPARK!").setParallelTaskNum(16).setLogger(&std::cout);
-
-  // Set and get configuration fields.
-  default_config.setDebugName("My default CPARK!");
-  std::cout << "The debug name of customized config is " << customized_config.getDebugName()
-            << std::endl;
-
   // Creates execution contexts.
   ExecutionContext default_context{};
-  ExecutionContext configured_context{default_config};
 
   // Creates a generator rdd, who holds 50 strings, each containing a number and a "hello".
-  std::cout << "The first split of the generator rdd contains the following elements: ";
+  std::cout << "Generator rdd : " << std::endl;
   auto generator_rdd = GeneratorRdd(
-      0, 50, [](auto x) { return x; }, &default_context);
-  for (const auto& x : generator_rdd.front()) {
-    std::cout << x << ", ";
-  }
-  std::cout << std::endl;
+      0, 50 + 1, [](auto x) { return x; }, &default_context);
+    printRdd(generator_rdd);
 
   auto even = [](int i) {
     return 0 == i % 2;
   };
 
   // Test for FilterRdd(const R& prev, Func func) constructor
-  auto filter_rdd_1 = FilterRdd(generator_rdd, even);
-  for (const auto& x : filter_rdd_1.front()) {
-    std::cout << x << ", ";
-  }
-  std::cout << std::endl;
+    std::cout << "Filter rdd (basic): " << std::endl;
+    auto filter_rdd_1 = FilterRdd(generator_rdd, even);
+    printRdd(filter_rdd_1);
 
   // Test for pipeline and Filter(Func func) operators
-  auto filter_rdd_2 = generator_rdd | Filter(even);
-  for (const auto& x : filter_rdd_1.front()) {
-    std::cout << x << ", ";
-  }
-  std::cout << std::endl;
+    std::cout << "Filter rdd (operators): " << std::endl;
+    auto filter_rdd_2 = generator_rdd | Filter(even);
+    printRdd(filter_rdd_2);
+
+    return 0;
 }
