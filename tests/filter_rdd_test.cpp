5f4b9bbc6877d45a99b35297bf519363262ef11b
chenlighten <xushichen@mail.ustc.edu.cn>
2023-11-07 23:08:04 -0500

    Seperate tests

---
+++ tests/filter_rdd_test.cpp
@@ -0,0 +1,26 @@
+
+#include "gtest/gtest.h"
+#include "cpark.h"
+#include "filter_rdd.h"
+#include "generator_rdd.h"
+
+using namespace cpark;
+
+TEST(rdd_suite, filter_rdd_correctness) {
+  ExecutionContext default_context{};
+  auto even = [](int i) {
+    return 0 == i % 2;
+  };
+  auto generator_rdd = GeneratorRdd(
+      0, 1000 + 1, [](auto x) { return x; }, &default_context);
+  auto filter_rdd = FilterRdd(generator_rdd, even);
+  int sum = 0, size = 0;
+  for (const concepts::Split auto& split : filter_rdd) {
+    for (const auto& x : split) {
+      sum += x;
+      size += 1;
+    }
+  }
+  EXPECT_EQ(501, size);
+  EXPECT_EQ(250500, sum);
+}
--- tests/rdd_test.cpp
+++ tests/rdd_test.cpp
@@ -34,22 +34,3 @@ TEST(rdd_suite, plain_rdd_dummy) {
 
   EXPECT_EQ(1, 1);
 }
-
-TEST(rdd_suite, filter_rdd_correctness) {
-  ExecutionContext default_context{};
-  auto even = [](int i) {
-    return 0 == i % 2;
-  };
-  auto generator_rdd = GeneratorRdd(
-      0, 1000 + 1, [](auto x) { return x; }, &default_context);
-  auto filter_rdd = FilterRdd(generator_rdd, even);
-  int sum = 0, size = 0;
-  for (const concepts::Split auto& split : filter_rdd) {
-    for (const auto& x : split) {
-      sum += x;
-      size += 1;
-    }
-  }
-  EXPECT_EQ(501, size);
-  EXPECT_EQ(250500, sum);
-}
