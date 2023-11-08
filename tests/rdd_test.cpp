735f10149e94e5998c5c0e5ebafc46e7727ab441
Alex Jiakai Xu <ax2155@columbia.edu>
2023-11-07 19:49:57 -0500

    Add gtest for Count action

--- tests/rdd_test.cpp
+++ tests/rdd_test.cpp
@@ -3,9 +3,12 @@
 #include "gtest/gtest.h"
 
 #include "cpark.h"
+
 #include "filter_rdd.h"
 #include "generator_rdd.h"
 #include "plain_rdd.h"
+
+#include "count.h"
 
 using namespace cpark;
 
@@ -52,4 +55,19 @@ TEST(rdd_suite, filter_rdd_correctness) {
   }
   EXPECT_EQ(501, size);
   EXPECT_EQ(250500, sum);
+}
+
+TEST(action_suite, count_test1) {
+  ExecutionContext default_context{};
+
+  const int N = 20000;
+  auto iota_view = std::views::iota(1, N + 1);
+  unsigned long long v_cnt = std::ranges::size(iota_view);
+
+  concepts::Rdd auto plain_rdd = PlainRdd(iota_view, &default_context);
+  unsigned long long c_cnt = plain_rdd | Count();
+
+  EXPECT_EQ(c_cnt, v_cnt);
+  EXPECT_EQ(N, c_cnt);
+
 }
