79b9f49d3168b44aed89bb15d317f8312355c247
chenlighten <xushichen@mail.ustc.edu.cn>
2023-11-14 12:04:53 -0500

    Finish MergeDiffView

--- include/merged_view.h
+++ include/merged_view.h
@@ -9,7 +9,8 @@ concept RangeOfView =
 }  // namespace concepts
 
 /**
- * A view combined from a sequence of views of the same type.
+ * A view combined from any number of views of the same type.
+ * It contains all the elements in these views with their original order.
  * @tparam R A range of views to be combined.
  */
 template <concepts::RangeOfView R>
@@ -27,13 +28,22 @@ public:
     using value_type = std::ranges::range_value_t<ViewType>;
 
   public:
+    /**
+     * Initialize the Iterator with:
+     * @param start_iterator The iterator to the first element.
+     * @param views A pointer to a vector of of views to be combined.
+     * @param start_index The index of the first view.
+     */
     Iterator(std::ranges::iterator_t<const ViewType> start_iterator, std::vector<ViewType>* views,
              std::vector<ViewType>::size_type start_index)
         : inner_iterator_{std::move(start_iterator)}, views_{views}, index_of_view_{start_index} {
+      // Move the inner iterator to the first element which is not an 'end'.
       while (index_of_view_ < std::ranges::size(*views_) &&
              inner_iterator_ == std::ranges::end((*views_)[index_of_view_])) {
         ++index_of_view_;
-        inner_iterator_ = std::ranges::begin((*views_)[index_of_view_]);
+        if (index_of_view_ < std::ranges::size(*views_)) {
+          inner_iterator_ = std::ranges::begin((*views_)[index_of_view_]);
+        }
       }
     }
 
@@ -43,10 +53,13 @@ public:
 
     Iterator& operator++() {
       ++inner_iterator_;
+      // Move the inner iterator to the first element which is not an 'end'.
       while (index_of_view_ < std::ranges::size(*views_) &&
              inner_iterator_ == std::ranges::end((*views_)[index_of_view_])) {
         ++index_of_view_;
-        inner_iterator_ = std::ranges::begin((*views_)[index_of_view_]);
+        if (index_of_view_ < std::ranges::size(*views_)) {
+          inner_iterator_ = std::ranges::begin((*views_)[index_of_view_]);
+        }
       }
       return *this;
     }
@@ -57,6 +70,11 @@ public:
       return old;
     }
 
+    /**
+     * Two Iterator-s are equal iff:
+     * They are both 'end' iterators, or if they have the same view sequence and point to the same
+     * element.
+     */
     bool operator==(const Iterator& other) const {
       if (isEnd() && other.isEnd()) {
         return true;
@@ -67,21 +85,30 @@ public:
 
     bool operator!=(const Iterator& other) const { return !(*this == other); }
 
-    Iterator& operator--() requires std::ranges::bidirectional_range<ViewType> {}
+    Iterator& operator--()
+      requires std::ranges::bidirectional_range<ViewType>
+    {}
 
   private:
     bool isEnd() const { return !(index_of_view_ < std::ranges::size(*views_)); }
 
   private:
+    // Iterator pointing to the current element.
     std::ranges::iterator_t<const ViewType> inner_iterator_{};
+    // The sequence of views to be combined.
     std::vector<ViewType>* views_{nullptr};
+    // Index of the view that has the current `inner_iterator_`.
     std::vector<ViewType>::size_type index_of_view_{0};
   };
 
 public:
+  /**
+   * Creates MergedSameView from a range of views to be combined.
+   */
   explicit MergedSameView(const R& views)
       : views_{std::ranges::begin(views), std::ranges::end(views)} {}
 
+  /** The iterator to the first element of the combined views. */
   constexpr auto begin() const {
     auto start_iterator = std::ranges::empty(views_) ? std::ranges::iterator_t<ViewType>{}
                                                      : std::ranges::begin(views_.front());
@@ -89,6 +116,7 @@ public:
                     static_cast<std::vector<ViewType>::size_type>(0)};
   }
 
+  /** The sentinel pass the last element of the combined views. */
   constexpr auto end() const {
     return Iterator{std::ranges::iterator_t<ViewType>{},
                     const_cast<std::vector<ViewType>*>(&views_), std::ranges::size(views_)};
@@ -98,21 +126,37 @@ private:
   std::vector<ViewType> views_;
 };
 
+/**
+ * A helper class that combines two views of different types into one.
+ * Their element types must be the same.
+ */
 template <std::ranges::view V1, std::ranges::view V2>
-requires std::same_as<std::ranges::range_value_t<V1>,
-                      std::ranges::range_value_t<V2>> class MergedTwoDiffView
-    : public std::ranges::view_interface<MergedTwoDiffView<V1, V2>> {
+  requires std::same_as<std::ranges::range_value_t<V1>, std::ranges::range_value_t<V2>>
+class MergedTwoDiffView : public std::ranges::view_interface<MergedTwoDiffView<V1, V2>> {
 public:
+  /**
+   * A lazily evaluated iterator that read elements combined from two different views.
+   */
   class Iterator : public std::forward_iterator_tag {
   public:
     using difference_type = std::ptrdiff_t;
     using value_type = std::ranges::range_value_t<V1>;
 
   private:
+    // Types of the iterators of the two original views.
     using OriginalIterator1 = std::ranges::iterator_t<const V1>;
     using OriginalIterator2 = std::ranges::iterator_t<const V2>;
 
   public:
+    /**
+    * Creates Iterator with:
+    * @tparam Iter Type of the start iterator.
+    * @param iterator The start iterator points to the first element.
+    * @param view1 The first view.
+    * @param view2 The second view.
+    * @param if_hold1 Whether the start iterator is from view1 or view2.
+    * Note that `if_hold1` must be consistent with the type of `Iter`.
+    */
     template <typename Iter>
     Iterator(Iter iterator, V1* view1, V2* view2, bool if_hold1)
         : hold1_{if_hold1}, view1_{view1}, view2_{view2} {
@@ -181,13 +225,18 @@ public:
   };
 
 public:
+  /**
+   * Creates MergedTwoDiffView from two views of possibily different types.
+   */
   MergedTwoDiffView(V1 view1, V2 view2) : view1_{std::move(view1)}, view2_{std::move(view2)} {}
 
+  /** The iterator to the first element of the combined views. */
   constexpr auto begin() const {
     return Iterator{std::ranges::begin(view1_), const_cast<V1*>(&view1_), const_cast<V2*>(&view2_),
                     true};
   }
 
+  /** The sentinel pass the last element of the combined views. */
   constexpr auto end() const {
     return Iterator{std::ranges::end(view2_), const_cast<V1*>(&view1_), const_cast<V2*>(&view2_),
                     false};
@@ -198,24 +247,13 @@ private:
   V2 view2_;
 };
 
-//template <typename... Args>
-//class MergedDiffView;
-//
-//template <std::ranges::view FirstView>
-//class MergedDiffView<FirstView> : public FirstView {
-//public:
-//  explicit MergedDiffView(FirstView&& first_view)
-//      : FirstView(std::forward<FirstView>(first_view)) {}
-//};
-//
-//template <std::ranges::view FirstView, std::ranges::view... OtherViews>
-//requires((std::is_same_v<std::ranges::range_value_t<FirstView>,
-//                         std::ranges::range_value_t<
-//                             OtherViews>>)&&...)
-//class MergedDiffView<FirstView, OtherViews...>: public MergedTwoDiffView<FirstView, MergedDiffView<OtherViews...>> {
-//public:
-//  explicit MergedDiffView(FirstView&& first_view, OtherViews&&... other_views)
-//      : MergedTwoDiffView<FirstView, MergedDiffView<OtherViews...>>{
-//            std::forward<FirstView>(first_view),
-//            MergedDiffView(std::forward<OtherViews>(other_views)...)} {}
-//};
+template <std::ranges::view FirstView>
+auto MergedDiffView(const FirstView& view) {
+  return view;
+}
+
+/** Creates a combined view from any number of possibly different views. */
+template <std::ranges::view FirstView, std::ranges::view... OtherViews>
+auto MergedDiffView(const FirstView& first_view, const OtherViews&... other_views) {
+  return MergedTwoDiffView{first_view, MergedDiffView(other_views...)};
+}
